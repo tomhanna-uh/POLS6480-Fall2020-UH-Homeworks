7e28c1c6d92ea37ef4c2a539b2f3764dff33ffb4
tomhanna-uh <tlhanna@uh.edu>
2020-10-28 15:59:18 -0500

    First commit

---
+++ homework4-question7-start.R
@@ -0,0 +1,20 @@
+rm(list=ls())
+setwd("C:/R Studio Files/Teaching/homework4-pols")
+
+library(RCurl)
+
+myfile <- getURL("https://raw.githubusercontent.com/kosukeimai/qss/master/PREDICTION/intrade08.csv")
+
+mydat <- read.csv(textConnection(myfile), header=T)
+
+head(mydat)
+
+mydat$prwin <- mydat$PriceD/100
+
+myfile <- getURL("https://raw.githubusercontent.com/kosukeimai/qss/master/DISCOVERY/pres08.csv")
+
+pres08 <-read.csv(textConnection(myfile), header=T)
+
+head(pres08)
+
+total<-merge(mydat, pres08, by=("state"))
